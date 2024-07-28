d629a1ed66725a8262f1c6550be809f30436202f
Kozmyn7 <kozmyn7@gmail.com>
2024-07-28 20:42:44 +0300

    Update autopopulate.js

--- autopopulate.js
+++ autopopulate.js
@@ -1,27 +1,12 @@
-function getQueryParams() {
-  let params = {};
-  let queryString = window.location.search.substring(1);
-  let regex = /([^&=]+)=([^&]*)/g;
-  let match;
-
-  while (match = regex.exec(queryString)) {
-    params[decodeURIComponent(match[1])] = decodeURIComponent(match[2]);
-  }
-  return params;
-}
-
 document.addEventListener("DOMContentLoaded", function() {
-  let params = getQueryParams();
-  let utmSource = params['utm_source'];
-  
+  let params = new URLSearchParams(window.location.search);
+  let utmSource = params.get('utm_source');
+
   if (utmSource) {
     let inputElement = document.querySelector('input[name="sursa trafic"]');
     if (inputElement) {
       inputElement.value = utmSource;
-    } else {
-      console.error('Input element not found');
     }
-  } else {
-    console.error('utm_source parameter not found in URL');
   }
 });
+
